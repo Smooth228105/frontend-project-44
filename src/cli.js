0a2d25e603c746acdc9e343ceaf4020899325fbe
Smooth228105 <Smooth228105@yandex.ru>
2024-02-21 10:59:53 +0300

    wip

---
+++ src/cli.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+
+export function greeting(){
+    console.log('Welcome to the Brain Games!');
+    const name = readlineSync.question('Your name: ');
+    console.log(`'Hello, ${name}!`)
+    return name
+}
+
+console.log(greeting());
